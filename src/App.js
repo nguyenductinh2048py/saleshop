6470fd4d7e8aae62b2873bd8bb951f6a02b1343f
nguyenductinh1998py <nguyenductinhtaiphuyen71@gmail.com>
2024-10-25 14:39:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ĐÂY LÀ CÁCH TAO HỌC DEPLOY TRANG WEB
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Deploy with github
         </a>
       </header>
     </div>
